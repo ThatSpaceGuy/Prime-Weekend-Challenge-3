8b80b8259e230befee3809bf8fe4950652de273a
Luis H. Matos <ReachLuis@Gmail.com>
2016-09-11 00:32:25 -0500

    0.3 - Get info, bundle, and send to server

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,10 +1,33 @@
 console.log('scripts.js sourced!');
 /// == Global Variable Declarations == ///
-
+var verbose = true; // if (verbose) {console.log('');}
 
 /// == Function Declarations == ///
 function sendCalc(){
+  if (verbose) {console.log('in sendCalc');}
+
+  var num1 = Number($('#numOne').val());
+  var operator = $('#operatorButton').val();
+  var num2 = Number($('#numTwo').val());
+
+  if (verbose) {console.log('Values: ', num1, operator, num2);}
+
+  // ajax post code that sends object to /routename route
+  var calcToSend={
+    numA: num1,
+    operation: operator,
+    numB: num2
+  };
   
+  $.ajax({
+    type: 'POST',
+    url: '/calculate',
+    data: calcToSend,
+    success: function( data ){
+      console.log( 'got this from server - ' + data );
+    }
+  }); // end Ajax post code
+
 }
 
 /// == JavaScript == ///
@@ -12,6 +35,6 @@ function sendCalc(){
 $(document).ready(function(){
   console.log('Document ready!');
 
-$('#equalsButton').on('click',sendCalc);
+  $('#equalsButton').on('click',sendCalc);
 
 }); // end document ready
