f1e2cd46a301cf734b877e69ec3e396d1af8825c
Luis H. Matos <ReachLuis@Gmail.com>
2016-09-11 00:16:43 -0500

    0.2.2 - Change layout, input types, and initial values

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -3,13 +3,15 @@ console.log('scripts.js sourced!');
 
 
 /// == Function Declarations == ///
-
+function sendCalc(){
+  
+}
 
 /// == JavaScript == ///
 
 $(document).ready(function(){
   console.log('Document ready!');
 
-
+$('#equalsButton').on('click',sendCalc);
 
 }); // end document ready
